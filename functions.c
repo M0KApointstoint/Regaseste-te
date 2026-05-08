076ed88deb18182bb743cfa2f302948a9abae465
Cristian Mocanu <mocanuc579@gmail.com>
2026-05-08 10:38:49 +0300

    Scrie functie care adauga o cheie in arbore de regasire + Coduri de iesire din functii.

--- functions.c
+++ functions.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include "functions.h"
 
 #define ALPHABET_LEN 26
 
@@ -18,5 +19,38 @@ struct trie_node *create_node(void)
 	}
 	new_node->is_terminal = 0;
 	return new_node;
+}
+
+int add_key_trie_helper(struct trie_node *node, char *key)
+{
+	if (!*key) {
+		if (node->is_terminal) {
+			return NO_SUCCESS;
+		}
+		node->is_terminal = 1;
+		return SUCCESS;
+	}
+	int index = *key - 'a';
+	if (!node->children[index]) {
+		node->children[index] = create_node();
+		if (!node->children[index]) {
+			return MALLOC_ERROR;
+		}
+	}
+	return add_key_trie_helper(node->children[index], key + 1);
+}
+
+int add_key_trie(struct trie_node **root_adr, char *key)
+{
+	if (!root_adr || !key || !*key) {
+		return INVALID_INPUT;
+	}
+	if (!*root_adr) {
+		*root_adr = create_node();
+		if (!*root_adr) {
+			return MALLOC_ERROR;
+		}
+	}
+	return add_key_trie_helper(*root_adr, key);
 }
 
--- functions.h
+++ functions.h
@@ -1,6 +1,13 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+enum {
+	INVALID_INPUT = -2,
+	MALLOC_ERROR = -1,
+	SUCCESS = 0,
+	NO_SUCCESS = 1,
+};
+
 struct trie_node;
 
 #endif
