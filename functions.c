36cd45a918f0074f714b1b2524c6a4051a245a67
Cristian Mocanu <mocanuc579@gmail.com>
2026-05-08 10:26:24 +0300

    Adauga functie care aloca un nod pentru arbore de regasire.

--- functions.c
+++ functions.c
@@ -1,7 +1,22 @@
+#include <stdlib.h>
+
 #define ALPHABET_LEN 26
 
 struct trie_node {
 	struct trie_node *children[ALPHABET_LEN];
 	int is_terminal;
 };
+
+struct trie_node *create_node(void)
+{
+	struct trie_node *new_node = malloc(sizeof(struct trie_node));
+	if (!new_node) {
+		return NULL;
+	}
+	for (int i = 0; i < ALPHABET_LEN; ++i) {
+		new_node->children[i] = NULL;
+	}
+	new_node->is_terminal = 0;
+	return new_node;
+}
 
